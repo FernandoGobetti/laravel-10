040d49bf30776970cdf091a923cff31d5af3e275
Fernando Gobetti <fernando.gobetti@hotmail.com.br>
2024-01-22 12:50:23 -0300

    Repository Support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -19,7 +19,7 @@ class SupportController extends Controller
 
     public function index(Request $request)
     {
-        $supports = $this->service->getAll($request);
+        $supports = $this->service->getAll($request->filter);
         return view('admin/supports/index', compact('supports'));
     }
 
